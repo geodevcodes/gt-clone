65e0a986ac8daf4c03a2e66839b2ab3e4431f773
geodevcode <geodevinstitute@gmail.com>
2025-11-27 13:45:46 +0100

    products images added

--- constants/images.ts
+++ constants/images.ts
@@ -1,9 +1,21 @@
 import comingsoon from "@/assets/images/comingsoon.jpeg";
 import trash from "@/assets/images/empty.png";
+import gtcontribution from "@/assets/images/gtcontribution.jpeg";
+import gtdollarfund from "@/assets/images/gtdollarfund.jpeg";
+import gtfund from "@/assets/images/gtfund.jpeg";
+import gtresidential from "@/assets/images/gtresidential.jpeg";
+import gtrsa from "@/assets/images/gtrsa.jpeg";
+import loan from "@/assets/images/loan.jpeg";
 import pension from "@/assets/images/pension.jpeg";
 
 export default {
   trash,
   pension,
   comingsoon,
+  loan,
+  gtfund,
+  gtdollarfund,
+  gtrsa,
+  gtcontribution,
+  gtresidential,
 };
