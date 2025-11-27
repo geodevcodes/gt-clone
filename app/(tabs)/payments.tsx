0daa1ba6f7ed853d6f2798c61aa5726dd33945bc
geodevcode <geodevinstitute@gmail.com>
2025-11-27 12:29:04 +0100

    payments screen refactored

--- app/(tabs)/payments.tsx
+++ app/(tabs)/payments.tsx
@@ -1,22 +1,43 @@
-import icons from "@/constants/icons";
+import { paymentData } from "@/lib/data/paymentData";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Payments = () => {
+  const onPressHandler = async () => {};
   return (
-    <SafeAreaView className="px-8 pt-8 pb-2">
+    <SafeAreaView className="px-5 pt-6 pb-8">
       <View className="flex flex-row items-center justify-between gap-3">
         <View className="flex flex-col gap-2">
-          <Text className="text-black font-bold text-3xl">Payments</Text>
+          <Text className="text-black font-bold text-3xl pb-3">Payments</Text>
         </View>
-        <Image
-          source={icons.filter}
-          resizeMode="contain"
-          className="w-[24px] h-[24px]"
-          tintColor="#2F7995"
-        />
       </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 80 }}
+      >
+        <View className="flex flex-col mt-4">
+          {paymentData.map((item, index) => (
+            <TouchableOpacity
+              onPress={onPressHandler}
+              key={index}
+              className="flex flex-row items-center justify-between py-3"
+            >
+              <View className="flex flex-row items-center gap-3">
+                <View className="bg-gray-300 p-2 rounded-full">
+                  {item.icon}
+                </View>
+                <Text className="text-base font-rubik-medium text-gray-600">
+                  {item.name}
+                </Text>
+              </View>
+              <Ionicons name="chevron-forward" size={18} color="black" />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
---
+++ lib/data/paymentData.tsx
@@ -0,0 +1,144 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Octicons from "@expo/vector-icons/Octicons";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
+
+export const paymentData = [
+  {
+    id: "buy_airtime_1",
+    name: "Buy airtime",
+    icon: (
+      <MaterialIcons name="signal-cellular-0-bar" size={20} color="#ff6600" />
+    ),
+  },
+  {
+    id: "buy_data_2",
+    name: "Buy data",
+    icon: (
+      <MaterialCommunityIcons
+        name="wifi-strength-4"
+        size={20}
+        color="#ff6600"
+      />
+    ),
+  },
+  {
+    id: "book_flight_3",
+    name: "Airlines, Travels & Transportation/Logistics",
+    icon: <Entypo name="aircraft-take-off" size={20} color="#ff6600" />,
+  },
+  {
+    id: "cable_tv_4",
+    name: "Cable TV",
+    icon: <MaterialIcons name="live-tv" size={20} color="#ff6600" />,
+  },
+  {
+    id: "capital_market_5",
+    name: "Capital Market & Investments",
+    icon: <Feather name="trending-up" size={20} color="#ff6600" />,
+  },
+  {
+    id: "distributor_agent_6",
+    name: "Distributors & Agent Payments",
+    icon: <SimpleLineIcons name="bag" size={20} color="#ff6600" />,
+  },
+  {
+    id: "electricity_water_7",
+    name: "Electricity & Water",
+    icon: <MaterialCommunityIcons name="water" size={20} color="#ff6600" />,
+  },
+  {
+    id: "entertainment_evoucher_8",
+    name: "Entertainment & E-Vouchers",
+    icon: <MaterialIcons name="bookmark" size={20} color="#ff6600" />,
+  },
+  {
+    id: "estates_associations_9",
+    name: "Estates & Associations",
+    icon: <MaterialIcons name="real-estate-agent" size={20} color="#ff6600" />,
+  },
+  {
+    id: "financial_institutions_10",
+    name: "Financial Institutions",
+    icon: <FontAwesome name="institution" size={20} color="#ff6600" />,
+  },
+  {
+    id: "government_taxes_11",
+    name: "Government Taxes and Levels",
+    icon: <FontAwesome6 name="sheet-plastic" size={20} color="#ff6600" />,
+  },
+  {
+    id: "infotechs_other_services_12",
+    name: "Infotechs & Other Services",
+    icon: <Entypo name="dots-three-horizontal" size={20} color="#ff6600" />,
+  },
+  {
+    id: "insurance_hmo_13",
+    name: "Insurance & HMO",
+    icon: <MaterialIcons name="security" size={20} color="#ff6600" />,
+  },
+  {
+    id: "internet_subscription_14",
+    name: "Internet Subscription",
+    icon: <FontAwesome6 name="internet-explorer" size={20} color="#ff6600" />,
+  },
+  {
+    id: "nibssebills_pay_15",
+    name: "NIBSSEBILLSPAY",
+    icon: <FontAwesome6 name="hornbill" size={20} color="#ff6600" />,
+  },
+  {
+    id: "paydirect_16",
+    name: "PAYDIRECT",
+    icon: <Octicons name="file-directory-symlink" size={20} color="#ff6600" />,
+  },
+  {
+    id: "religious_institutions_ngo_17",
+    name: "Religious Institutions & NGO",
+    icon: <MaterialCommunityIcons name="church" size={20} color="#ff6600" />,
+  },
+  {
+    id: "remita_18",
+    name: "Remita",
+    icon: <AntDesign name="trademark" size={20} color="#ff6600" />,
+  },
+  {
+    id: "schools_professional_bodies_19",
+    name: "Schools & Professional Bodies",
+    icon: <MaterialIcons name="school" size={20} color="#ff6600" />,
+  },
+  {
+    id: "shipping_line_payment_20",
+    name: "Shipping Line Payment",
+    icon: <MaterialCommunityIcons name="book" size={20} color="#ff6600" />,
+  },
+  {
+    id: "sports_gaming_21",
+    name: "Sports and Gaming",
+    icon: (
+      <MaterialCommunityIcons name="tennis-ball" size={20} color="#ff6600" />
+    ),
+  },
+  {
+    id: "toll_fees_lcc_22",
+    name: "Toll Fees - LCC",
+    icon: <FontAwesome6 name="car-rear" size={20} color="#ff6600" />,
+  },
+  {
+    id: "utilities_23",
+    name: "Utilities",
+    icon: <MaterialIcons name="pentagon" size={20} color="#ff6600" />,
+  },
+  {
+    id: "visa_fee_payment_24",
+    name: "VISA Fee Payment",
+    icon: (
+      <MaterialCommunityIcons name="content-copy" size={20} color="#ff6600" />
+    ),
+  },
+];
