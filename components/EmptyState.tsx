ab687f335f00ff94f6f4d0d52f5fc937126868e9
geodevcode <geodevinstitute@gmail.com>
2025-11-28 12:53:05 +0100

    EmptyState component refactored

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,29 +1,39 @@
-import images from "@/constants/images";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
 interface EmptyStateProps {
   title: string;
   subTitle: string;
+  imageUrl: any;
+  buttonName: string;
+  onPress?: () => void;
 }
-const EmptyState = ({ title, subTitle }: EmptyStateProps) => {
+const EmptyState = ({
+  title,
+  subTitle,
+  imageUrl,
+  buttonName,
+  onPress,
+}: EmptyStateProps) => {
   return (
     <View className="justify-center items-center px-4 ">
       <Image
-        source={images.trash}
+        source={imageUrl}
         resizeMode="contain"
-        className="w-[270px] h-[215px]"
+        className="w-[250px] h-[195px]"
       />
-      <Text className="text-center font-spaceMono-regular font-bold text-3xl mt-2">
-        {title}
+      <Text className="text-center font-semibold text-2xl mt-6">{title}</Text>
+      <Text className="text-center text-sm text-slate-500 mt-3 mb-2">
+        {subTitle}
       </Text>
-      <Text className="font-semibold text-base text-gray-100">{subTitle}</Text>
-
       <TouchableOpacity
+        onPress={onPress}
         activeOpacity={0.7}
-        className="bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center w-full my-5"
+        className="bg-[#ff6600] rounded-lg flex flex-row justify-center items-center min-w-[160px] my-5"
       >
-        <Text className="text-white font-semibold text-xl ">Reload</Text>
+        <Text className="text-white p-2 py-3 text-center font-semibold text-xl ">
+          {buttonName}
+        </Text>
       </TouchableOpacity>
     </View>
   );
